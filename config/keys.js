37258570cf4899ed0fd1e2cf643ee1f82c155314
Zemichael Mehretu <MY_NAME@example.comzemichaelmd@gmail.com>
2021-10-23 10:48:47 +0300

    Done Jwtauth

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
-module.exports ={
-    mongoURI:'mongodb+srv://zemichaelmd:lidlove00@cluster0.o2jpv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-}
+module.exports = {
+  mongoURI:
+    "mongodb+srv://zemichaelmd:lidlove00@cluster0.o2jpv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  secretOrKey: "bigSecret",
+};
--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
-const users = require ('./routes/api/users');
-const profile = require ('./routes/api/profile');
-const post = require ('./routes/api/post');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const post = require('./routes/api/post');
 
 const app = express();
 
+// BodyParser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //DB config
-const db = require ('./config/keys').mongoURI;
+const db = require('./config/keys').mongoURI;
 
 //connect to MongoDB
 mongoose
-    .connect(db)
-    .then(console.log("MongoDB Connected"))
-    .catch(e=>console.log(e))
+  .connect(db)
+  .then(console.log('MongoDB Connected'))
+  .catch((e) => console.log(e));
 
-app.get('/', (req, res)=>res.send('Hello World!'));
+//passport middleware
+app.use(passport.initialize());
+
+//passport config
+require('./config/passport')(passport);
 
 //use Routes
 app.use('/api/users', users);
@@ -25,4 +35,4 @@ app.use('/api/post', post);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>console.log(`Server Running on Port ${port}`))
+app.listen(port, () => console.log(`Server Running on Port ${port}`));
